0d6d73ba04a497a177566124574144f14b4d597d
LunashaGit <lunamuylkens@gmail.com>
2023-05-16 11:54:08 +0200

    Init ShoppingList Components

--- shoppingList/src/app/app.module.ts
+++ shoppingList/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
-
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 @NgModule({
-  declarations: [		
+  declarations: [	
     AppComponent,
       HeaderComponent,
-      RecipesComponent
+      RecipesComponent,
+      RecipeListComponent,
+      RecipeItemComponent,
+      RecipeDetailComponent,
+      ShoppingListComponent
    ],
   imports: [
     BrowserModule,
