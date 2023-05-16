d9ae63a824a7027fe34c8feb2b4021331398da5b
LunashaGit <lunamuylkens@gmail.com>
2023-05-16 09:24:27 +0200

    Add Ex & Ex1 folder

---
+++ Exercices/Ex1/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+      WarningAlertComponent,
+      SuccessAlertComponent
+   ],
+  imports: [
+    // Import Packages
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
