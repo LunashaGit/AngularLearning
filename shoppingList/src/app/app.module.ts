f3c26e55bf71654af8f8997d92260063367a0a40
LunashaGit <lunamuylkens@gmail.com>
2023-05-16 11:57:06 +0200

    add shoppingList to app.module

--- shoppingList/src/app/app.module.ts
+++ shoppingList/src/app/app.module.ts
@@ -9,15 +9,17 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
       HeaderComponent,
       RecipesComponent,
       RecipeListComponent,
       RecipeItemComponent,
       RecipeDetailComponent,
-      ShoppingListComponent
+      ShoppingListComponent,
+      ShoppingEditComponent
    ],
   imports: [
     BrowserModule,
