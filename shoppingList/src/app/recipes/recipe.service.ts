6e0dd2443eaf0bc931e255a06c31af32c581cf6d
LunashaGit <lunamuylkens@gmail.com>
2023-05-17 13:32:15 +0200

    Add ingredients to recipes

--- shoppingList/src/app/recipes/recipe.service.ts
+++ shoppingList/src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +14,8 @@ export class RecipeService {
     new Recipe(
       'Vegan Sandwich',
       'This is a vegan Sandwich',
-      'https://assets.bonappetit.com/photos/5e4c58a55f0567000a32b827/1:1/w_3635,h_3635,c_limit/HLY_Vegan%20Alfredo_Lede.jpg'
+      'https://assets.bonappetit.com/photos/5e4c58a55f0567000a32b827/1:1/w_3635,h_3635,c_limit/HLY_Vegan%20Alfredo_Lede.jpg',
+      [new Ingredient('Potato', 2), new Ingredient('Salade', 1)]
     ),
   ];
 
